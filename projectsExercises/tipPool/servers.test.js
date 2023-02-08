a2bbf75dc11b3148d18b0ac364208c59c1ca347d
Tim <tim@Pearl>
2023-02-08 13:24:22 -0800

    finished servers.test.js

--- projectsExercises/tipPool/servers.test.js
+++ projectsExercises/tipPool/servers.test.js
@@ -1,8 +1,9 @@
+beforeEach(function () {
+    // initialization logic
+    serverNameInput.value = 'Alice';
+});
+
 describe("Servers test (with setup and tear-down)", function () {
-    beforeEach(function () {
-        // initialization logic
-        serverNameInput.value = 'Alice';
-    });
 
     it('should add a new server to allServers on submitServerInfo()', function () {
         submitServerInfo();
@@ -11,7 +12,26 @@ describe("Servers test (with setup and tear-down)", function () {
         expect(allServers['server' + serverId].serverName).toEqual('Alice');
     });
 
-    afterEach(function () {
-        // teardown logic
-    });
+
+
+});
+
+describe("Update Server test (with setup and clean up)", function () {
+
+    it("should add a new server to serverTBody on updateServerTable()", function () {
+        submitServerInfo();
+        updateServerTable();
+        let key = String(Object.keys(allServers).length)
+        let tdSName = document.getElementById("server" + key)
+        expect(tdSName.firstChild.textContent).toEqual("Alice")
+        expect(tdSName.lastChild.textContent).toEqual("$0.00")
+    })
+})
+
+afterEach(function () {
+    serverForm.reset()
+    serverNameInput.value = ""
+    document.getElementById("server1").remove()
+    allServers = {}
+    serverId = 0
 });

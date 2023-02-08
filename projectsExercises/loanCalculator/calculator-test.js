860dbbe884f97c8bc13554c7449047a440963fbc
Tim <tim@Pearl>
2023-02-08 00:22:55 -0800

    finished loancalc

--- projectsExercises/loanCalculator/calculator-test.js
+++ projectsExercises/loanCalculator/calculator-test.js
@@ -1,11 +1,31 @@
 
 it('should calculate the monthly rate correctly', function () {
-    // ...
+    let values = { "amount": 17500, "years": 3, "rate": 0.2999 }
+    expect(calculateMonthlyPayment(values)).toEqual(742.81)
+
+    values = { "amount": 800000, "years": 30, "rate": 0.03 }
+    expect(calculateMonthlyPayment(values)).toEqual(3372.83)
 });
 
 
 it("should return a result with 2 decimal places", function () {
-    // ..
+    let values = { "amount": 10000, "years": 5, "rate": 0.2999 }
+    expect(calculateMonthlyPayment(values)).toEqual(323.47)
+    expect(calculateMonthlyPayment(values)).toBeCloseTo(323.47, 2)
+    expect(calculateMonthlyPayment(values)).not.toEqual(323.473)
+    expect(calculateMonthlyPayment(values)).not.toEqual(323.5)
 });
 
-  /// etc
+
+it("shouldn't be able to calculate strings as inputs", function () {
+    let values = { "amount": "sdfg", "years": 35, "rate": 0.75 }
+    expect(calculateMonthlyPayment(values)).toEqual(NaN)
+})
+
+
+it("shouldn't be able to calculate strings as inputs", function () {
+    let values = { "amount": "sdfg", "years": 35, "rate": 0.75 }
+    expect(() => checkInputs(values)).toThrowError("Invalid Input")
+})
+
+
--- projectsExercises/loanCalculator/calculator.js
+++ projectsExercises/loanCalculator/calculator.js
@@ -11,9 +11,9 @@ window.addEventListener('DOMContentLoaded', function () {
 
 function getCurrentUIValues() {
     return {
-        amount: +(document.getElementById("loan-amount").value),
-        years: +(document.getElementById("loan-years").value),
-        rate: +(document.getElementById("loan-rate").value),
+        amount: + document.getElementById("loan-amount").value,
+        years: + document.getElementById("loan-years").value,
+        rate: + document.getElementById("loan-rate").value,
     }
 }
 
@@ -21,20 +21,45 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+    document.getElementById("loan-amount").value = 10000
+    document.getElementById("loan-years").value = 5
+    document.getElementById("loan-rate").value = 0.2999
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+    let values = getCurrentUIValues()
+    checkInputs(values)
+    let monthlyPayments = calculateMonthlyPayment(values)
+    updateMonthly(monthlyPayments)
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+    let principle = values.amount;
+    let interest = values.rate / 12;
+    let n = values.years * 12;
+    let monthlyPayments = (principle * interest) / (1 - (1 + interest) ** (-n))
+    monthlyPayments = Math.round(monthlyPayments * 100) / 100
+    return monthlyPayments
+}
+
+function checkInputs(values) {
+    if (isNaN(values.amount)) {
+        throw new Error("Invalid Input")
+    }
+    if (isNaN(values.years)) {
+        throw new Error("Invalid Input")
+    } if (isNaN(values.rate)) {
+        throw new Error("Invalid Input")
+    }
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+    document.getElementById("monthly-payment").innerText = `$${monthly}`
 }

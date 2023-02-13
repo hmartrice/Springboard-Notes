bbc96751a4365ba30a0d67a0f77b1f5826102ebe
Tim <tim@Pearl>
2023-02-13 08:27:08 -0800

    didnt finish optional

--- projectsExercises/tipPool/payments.test.js
+++ projectsExercises/tipPool/payments.test.js
@@ -1,3 +1,13 @@
-describe("Testing submitPaymentInfo", function () {
-
+beforeAll(function () {
+    billAmtInput.value = 100
+    tipAmtInput.value = 50
 })
+
+describe("Testing submitPaymentInfo", function () {
+    let event = ''
+    submitPaymentInfo(event);
+    it("bill and tip input should be changed back to zero", function () {
+        expect(billAmtInput.value).toEqual("")
+        expect(tipAmtInput.value).toEqual("")
+    })
+})      

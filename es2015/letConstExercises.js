74ec385258a645f1120658b9268086c34c106c3e
Tim <tim@Pearl>
2023-02-16 15:29:06 -0800

    letConstExercises.js

---
+++ es2015/letConstExercises.js
@@ -0,0 +1,8 @@
+//ES5 Global Constants
+let PI = 3.14
+PI = 42
+
+//Var has a functional scope and can be redeclared/reassigned
+//Let like const has a block scope. Let can be reassigned but not redeclared
+//Const has a block scope and can't be reassigned or redeclared. It is however mutable.
+//Hoisting is when you can access a declared variable that is not assigned for its undefined value. 
